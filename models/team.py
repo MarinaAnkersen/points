bd9f844e7f02e4ead38b7c338aefe0ad88133282
Maryna Storchak <mstorchak13@gmail.com>
2020-05-08 16:52:26 +0100

    Add team model and resource.

---
+++ models/team.py
@@ -0,0 +1,63 @@
+
+from db import db
+
+class TeamModel(db.Model):
+    __tablename__ = 'championship'
+
+    id = db.Column(db.Integer, primary_key=True)
+    team_name = db.Column(db.String(80))
+    spi = db.Column(db.Float(precision=1))
+    off = db.Column(db.Float(precision=1))
+    defi = db.Column(db.Float(precision=1))
+    goal_dif = db.Column(db.Integer)
+    pts = db.Column(db.Integer)
+    relegated = db.Column(db.Integer)
+    make_from_playoffs = db.Column(db.Integer)
+    promoted = db.Column(db.Integer)
+    win_championship = db.Column(db.Integer)
+
+    def __init__(self, team_name, spi=None, off=None, defi=None,
+    goal_dif=None,pts=None,relegated=None,make_from_playoffs=None,
+    promoted=None,win_championship=None):
+        self.team_name = team_name
+        self.spi = spi
+        self.off = off
+        self.defi = defi
+        self.goal_dif = goal_dif
+        self.pts = pts
+        self.relegated = relegated
+        self.make_from_playoffs = make_from_playoffs
+        self.promoted = promoted
+        self.win_championship = win_championship
+
+    def json(self):
+        return {'id': self.id,
+        'team_name': self.team_name,
+        'spi': self.spi,
+        'off': self.off,
+        'defi': self.defi,
+        'goal_dif': self.goal_dif,
+        'pts': self.pts,
+        'relegated': self.relegated,
+        'make_from_playoffs': self.make_from_playoffs,
+        'promoted': self.promoted,
+        'win_championship': self.win_championship}
+
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
+
+    @classmethod
+    def find_by_name(cls, team_name):
+        return cls.query.filter_by(team_name=team_name).first()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
+
+# self.team_name = team_name
---
+++ resources/team.py
@@ -0,0 +1,56 @@
+from flask_restful import  Resource, reqparse
+from flask_jwt_extended import (
+jwt_required,
+jwt_optional,
+get_jwt_identity)
+from models.team import TeamModel
+
+
+class Team(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('spi',type=float)
+    parser.add_argument('off',type=float)
+    parser.add_argument('defi',type=float)
+    parser.add_argument('goal_dif',type=int)
+    parser.add_argument('pts',type=int)
+    parser.add_argument('relegated',type=int)
+    parser.add_argument('make_from_playoffs',type=int)
+    parser.add_argument('promoted',type=int)
+    parser.add_argument('win_championship',type=int)
+    
+    @jwt_required
+    def get(self, team_name):
+        team = TeamModel.find_by_name(team_name)
+        if team:
+            return team.json()
+        return {'message': 'It doesnt exist '}, 404
+
+
+    def post(self, team_name):
+        if TeamModel.find_by_name(team_name):
+            return {'message': "a team '{}' already exists".format(team_name)}, 400
+        data = Team.parser.parse_args()
+        team = TeamModel(team_name, **data)
+
+        try:
+            team.save_to_db()
+        except:
+            return {"message": "An error has occured"}, 500 #internale server error
+
+        return team.json(), 201
+
+
+class TeamList(Resource):
+    @jwt_optional
+    def get(self):
+        user_id = get_jwt_identity()
+        teams = [team.json() for team in TeamModel.find_all()]
+        if user_id:
+            return {'teams': teams}, 200
+        return {'teams': [team['team_name'] for team in teams],
+        'message': 'More data available if you log in'}, 200
+
+
+
+
+        # {"items": list(map(lambda x: x.json(), ItemModel.query.all()))}
